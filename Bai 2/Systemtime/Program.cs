0c5645d54bc43b5dfd061751c6f47147a2b17062
Palamute <hydro2085@gmail.com>
2023-12-08 18:29:58 +0700

    real time

---
+++ Bai 2/Systemtime/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Systemtime
+{
+class Program
+{
+    private static void Main(string[] args)
+    {
+        DateTime localTime = DateTime.Now;
+        Console.WriteLine("thoi gian hien tai la: " + localTime);
+    }
+}
+}
