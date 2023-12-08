e1c609c802b0b7957f142bc4a37f4dd783a44341
Palamute <hydro2085@gmail.com>
2023-12-08 18:47:44 +0700

    new

---
+++ Bai 2/UsingOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace UsingOperator
+{
+class Program
+{
+    private static void Main(string[] args)
+    {
+        float width;
+        float height;
+        Console.WriteLine("nhap gia tri cgo hinh chu nhat");
+        Console.WriteLine("nhap chieu rong: ");
+        width = float.Parse(Console.ReadLine());
+        Console.WriteLine("nhhap chieu cao: ");
+        height = float.Parse(Console.ReadLine());
+        Console.WriteLine("Dien tich hinh chu nhat la: " + width*height);
+        
+    }
+}
+}
