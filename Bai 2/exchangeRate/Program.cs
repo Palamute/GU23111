6b3364089629fd482235ce6e6c2e00e4b082f9b7
Palamute <hydro2085@gmail.com>
2023-12-08 19:14:42 +0700

    Exchange Rate

---
+++ Bai 2/exchangeRate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace exchangeRate
+{
+class Program
+{
+    private static void Main(string[] args)
+    {
+        float ExchangRate = 23000;
+        float VND;
+        Console.WriteLine("nhap so tien VND: ");
+        VND = float.Parse(Console.ReadLine());
+        Console.WriteLine("so tien cua ban la: " + VND*ExchangRate);
+    }
+}
+}
