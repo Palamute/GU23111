eb67404fd882b5dbace2a0ceb15fc1e5eb6f2830
Palamute <hydro2085@gmail.com>
2023-12-11 17:57:19 +0700

    new

---
+++ Bai 2/Ptbac1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine("Nhap so a");
+       Double a = Convert.ToDouble(Console.ReadLine());
+       Console.WriteLine("Nhap so b");
+       Double b = Convert.ToDouble(Console.ReadLine());
+
+       if (a!=0)
+       {
+        Console.WriteLine("PT co nghiem" + -b/a);
+       }
+       else{
+        if(b==0) Console.WriteLine("PT vo so nghiem");
+        else{
+            Console.WriteLine("PT vo nghiem");
+        }
+       }
+    }
+}
