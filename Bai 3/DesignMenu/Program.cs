f15b04903ab843b7664a11fd68a4c9e20ceaf26c
Palamute <hydro2085@gmail.com>
2023-12-13 21:43:05 +0700

    new

---
+++ Bai 3/DesignMenu/Program.cs
@@ -0,0 +1,47 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int choise = -1;
+        while (choise!=0)
+        {
+            Console.WriteLine("Nhap mot ki tu tu ban phim");
+            choise = int.Parse(Console.ReadLine());
+            switch (choise)
+            {
+                case 1:
+                {
+                    Console.WriteLine("**********");
+                    Console.WriteLine("*********");
+                    Console.WriteLine("********");
+                    Console.WriteLine("*******");
+                    Console.WriteLine("******");
+                    Console.WriteLine("*****");
+                    Console.WriteLine("****");
+                    Console.WriteLine("***");
+                    Console.WriteLine("**");
+                    Console.WriteLine("*");
+                }
+                break;
+                case 2:
+                {
+                Console.WriteLine("**********");
+                Console.WriteLine("**********");
+                Console.WriteLine("**********");
+                Console.WriteLine("**********");
+                Console.WriteLine("**********");
+                }
+                break;
+                case 3:
+                {
+                    Console.WriteLine("**********");
+                    Console.WriteLine("**********");
+                    Console.WriteLine("**********");
+                }
+                break;
+                default: Console.WriteLine("Nhap lai");
+                break;
+            }
+        }
+    }
+}
---
+++ Bai 3/loopEach/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static int[] mang ={0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    private static void Main(string[] args)
+    {
+        foreach (var item in mang)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
