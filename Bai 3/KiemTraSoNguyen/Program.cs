f1009ed99eba89c9d5adc7151a5b4949ae82538e
Palamute <hydro2085@gmail.com>
2023-12-15 18:08:24 +0700

    new

---
+++ Bai 3/KiemTraSoNguyen/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int number;
+        Console.WriteLine("enter a number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+        if (number < 2)
+        Console.WriteLine(number + "is not a prime");
+        else{
+            int i = 2;
+            bool check = true;
+            while (i <= Math.Sqrt(number))
+            {
+                if (number % i == 0)
+                {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+            {
+                Console.WriteLine(number + " is a prime number");
+            }
+            else{
+                Console.WriteLine(number + " is not a prime number");
+            }
+        }
+    }
+}
