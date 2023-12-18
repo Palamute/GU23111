e1590c1ef6dbf012ad3e4f2f988a5d99a024ac76
Palamute <hydro2085@gmail.com>
2023-12-18 20:56:15 +0700

    new

---
+++ GU1-W04/AddList/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sum = 0;
+        int [] myList = {1,2,3,4,5,6,7,8,9,10};
+        Console.WriteLine("tong cua mang la: ");
+        for (int i = 0; i <= myList.Length-1; i++)
+        {
+            sum += myList[i];
+        }
+        Console.WriteLine(sum);
+    }
+}
---
+++ GU1-W04/ArrayList/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int [] myList = new int [10];
+        int [] myList2 = [1,2,3,4,5,6,7,8,9,10];
+        //truy xuất dữ liệu mảng
+        //tên mảng chỉ số mảng
+        int a = myList[0]; //lấy ra
+        myList[0] = 0;   // đưa vào
+
+        Console.WriteLine(myList2.Length); //độ dài của mảng
+        Console.WriteLine(myList2.Length-1); //chỉ số cuối cùng của mảng
+    }
+}
