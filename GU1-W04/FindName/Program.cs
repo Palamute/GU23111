0821988996c63cf0d76a31c65894c3b00a5a8662
Palamute <hydro2085@gmail.com>
2023-12-18 21:35:56 +0700

    new

---
+++ GU1-W04/FindName/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] nameList = {"Nam", "Trung", "Kien", "Binh", "Lam", "Bac"};
+        Console.WriteLine("Nhap ten ban: ");
+        string name = Console.ReadLine();
+        bool istrue = false;
+        for (int i = 0; i <= nameList.Length-1; i++)
+        {
+            if(nameList[i].Equals(name))
+            {
+                Console.WriteLine($"co ten trong mang va vi tri {i+1}");
+                istrue = true;
+                break;
+            }
+        }
+        if(!istrue)
+        {
+            Console.WriteLine("khong co ten");
+        }
+    }
+}
