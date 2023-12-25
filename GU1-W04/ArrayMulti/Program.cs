843b894f567709679fc122aa855bb5fc40044652
Palamute <hydro2085@gmail.com>
2023-12-25 18:04:14 +0700

    new

---
+++ GU1-W04/ArrayMulti/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] array = {{1,2,3,4},
+                         {1,2,3,4}, 
+                         {5,6,7,8}, 
+                         {9,10,11,12}};
+       int max = array[0,0];
+       for(int i = 0; i < array.GetLength(0); i++)
+       {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if(array[i,j] > max)
+            {
+                max = array[i,j];
+            }
+        }
+       }
+       Console.WriteLine("gia tri lon nhat cua mang la" + max);
+    }
+}
---
+++ GU1-W04/ArrayMultiKey/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.NetworkInformation;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so ham");
+        int row = int.Parse(Console.ReadLine());
+        Console.WriteLine("nhap so cot");
+        int column = int.Parse(Console.ReadLine());
+        Console.WriteLine("nhap cac phan tu cua mang: ");
+        int[,] array = new int [row,column];
+        for (int i = 0; i< row; i++)
+        {
+            for (int j = 0; j <column; j++)
+            {
+                array[i,j] = int.Parse(Console.ReadLine());
+            }
+        }
+        for (int i = 0; i < row; i++)
+        {
+            Console.WriteLine("\n");
+            for (int j = 0; j < column; j++)
+            {
+                Console.Write(array[i,j] + "\t");
+            }
+        }
+        //gia tri lon nhat cua mang
+        int max = array[0,0];
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                if(max<array[i,j]);
+                {
+                max = array[i,j];
+                }
+            }
+        }
+        Console.WriteLine("gia tri lon nhat la: ");
+
+        //tinh tong
+        int total = 0;
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                if(i==j){
+                    total += array[i,j];
+                }
+            }
+        }
+        Console.WriteLine("gia tri tong duong cheo la: " + total);
+    }
+}
