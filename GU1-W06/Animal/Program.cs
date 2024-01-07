155593b3d803ea25894580030493397c9dd89edd
Palamute <hydro2085@gmail.com>
2024-01-07 21:08:25 +0700

    new

---
+++ GU1-W06/Animal/Program.cs
@@ -0,0 +1,10 @@
+﻿using Animal;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Cat cat = new Cat("20kg", "1.2", "kitty");
+        cat.PrintInfo();
+    }
+}
