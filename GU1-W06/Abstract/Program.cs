e9292bfa9ceac54e4ea5f3f1ede7f53030560248
Palamute <hydro2085@gmail.com>
2024-01-15 21:12:31 +0700

    new

---
+++ GU1-W06/Abstract/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+}
+public  abstract class Enemy
+{
+    public abstract void Attack();
+}
+public class Dragon : Enemy
+{
+    public override void Attack()
+    {
+        Console.WriteLine("Cao can xe");
+    }
+}
+public class Robot:Enemy
+{
+    public override void Attack()
+    {
+        Console.WriteLine("Ban sung");
+    }
+}
+public class Man:Enemy
+{
+   public override void Attack()
+    {
+        Console.WriteLine("Chem");
+    } 
+}
+public abstract class Vehicle
+{
+    
+}
---
+++ GU1-W06/ClassBase/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+    }
+}
+
+class Animal
+{
+    int eyes = 2;
+    int mouth = 1;
+    int legs = 4;
+}
+class Cat : Animal
+{
+    int weight = 4;
+}
+class Dog : Animal
+{
+    int weight = 10;
+}
